84034f14fe646919066d00c8c9185d876beb184d
angeliaginting <52189342+angeliaginting@users.noreply.github.com>
2019-07-15 17:46:58 +0700

    Add files via upload

---
+++ p5a_perulangan/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int Umur[5] = {18,19,20,21,22};
+int n, hasil;
+
+int main()
+{
+    int a,b,c,d,e;
+    a=20;
+    b=19;
+    hasil = a+b;
+
+    cout<<hasil<<endl;
+
+    for(n=0; n<5; n++){
+        hasil += Umur[0] + Umur[1] + Umur [2] + Umur [3] + Umur [4];
+    }
+
+    cout << hasil << endl;
+
+    getch();
+    return 0;
+}

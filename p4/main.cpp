a52126193445e764e9603397778e7a245ba0afdd
angeliaginting <52189342+angeliaginting@users.noreply.github.com>
2019-07-15 17:39:05 +0700

    Add files via upload

---
+++ p4/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+    int i;
+    //for menaik
+    for(i=1; i<=10; i++){
+        cout << i << " Looping" << endl;
+    }
+
+    cout << endl;
+
+    //for menurun
+    for(i=10; i>=1; i--){
+        cout << i << " Looping" << endl;
+    }
+
+    getch();
+    return 0;
+}
